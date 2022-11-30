81987a77b1f165b6d8d0350baf1a6e4d2182c14a
pavelDoldin <doldin86@icloud.com>
2022-11-30 21:35:30 +0500

    if else

---
+++ Example005_if_else/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+Console.WriteLine("Введите ваше имя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "маша")
+{
+    Console.WriteLine("Ура, это же Маша!");
+}
+else
+{
+    Console.Write("Привет: ");
+    Console.WriteLine(username);
+}
